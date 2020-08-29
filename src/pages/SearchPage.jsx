e1f00f3aae9ebff2cb21a7940a692019c67ec3e1
MakkiAnjum <muhammadmakkianjum@yahoo.om>
2020-08-29 13:35:06 +0500

    Search page results + styling

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -68,7 +68,33 @@ function SearchPage() {
           </div>
         </div>
       </div>
-      <div className="searchPage__results"></div>
+      {term && (
+        <div className="searchPage__results">
+          <p className="searchPage__resultCount">
+            About {data?.searchInformation.formattedTotalResults} results (
+            {data?.searchInformation.formattedSearchTime} seconds) for {term}
+          </p>
+          {data?.items.map((item) => (
+            <div className="searchPage__result">
+              <a href={item.link} className="searchPage__resultLink">
+                {item.pagemap?.cse_image?.length > 0 &&
+                  item.pagemap?.cse_image[0]?.src && (
+                    <img
+                      className="searchPage__resultImage"
+                      src={item.pagemap?.cse_image[0]?.src}
+                      alt="Image"
+                    />
+                  )}
+                {item.displayLink}
+              </a>
+              <a href={item.displayLink} className="searchPage__resultTitle">
+                <h2>{item.title}</h2>
+              </a>
+              <p className="searchPage__resultSnippet">{item.snippet}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
