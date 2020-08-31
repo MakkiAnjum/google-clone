262a2c24056a66ff5084aaa5eb99b1b2a853d494
MakkiAnjum <muhammadmakkianjum@yahoo.om>
2020-08-31 11:11:29 +0500

    Updates

---
+++ precache-manifest.a0654d23563e9fec8aeefb4f2c3404d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4ec946c558cb41d07f0431aabb7ea9",
+    "url": "/google-clone/index.html"
+  },
+  {
+    "revision": "58cb1f47660f8e92953e",
+    "url": "/google-clone/static/css/main.cf26e136.chunk.css"
+  },
+  {
+    "revision": "6b6b610f91b037360584",
+    "url": "/google-clone/static/js/2.1b025511.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google-clone/static/js/2.1b025511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58cb1f47660f8e92953e",
+    "url": "/google-clone/static/js/main.05ade7fe.chunk.js"
+  },
+  {
+    "revision": "53e6f9d7fcbadc32d4de",
+    "url": "/google-clone/static/js/runtime-main.20aa3e5d.js"
+  }
+]);
